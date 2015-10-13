03ca30fecbf09e1a757f823fe0df00dfe730d922
Steven Van Sant <kipomaha@gmail.com>
2015-10-13 11:17:45 -0500

    Added comments and readability changes
    Started writing comments for the data Markup language
    Added white space and returns

--- blogs/javascripts/ktml.js
+++ blogs/javascripts/ktml.js
@@ -1,14 +1,23 @@
-// JavaScript Documentalert 
+// JavaScript 
 "use strict";
 var charMat = /\w+/g;
 var numberMatch = /\.\d+|\d+\.\d+|\d+/g, indexUnitMatch = /\d+px|\.\d+|\d+\.\d+|\d+cm|\.\d+|\d+\.\d+|\d+in/g;
+
+
 // --dML (Data Markup Language)
-var dML = {
-	"getData": /\w+(?=\|)/g,
-	"getObject": /\{\w+\(\w+\)/g,
-	"getPart": /\w+(?=\.|\()/g,
-	"getPath": /\{\w+(?=\()/g,
-	"getElement": /\w+(?=\))/g
+	// A set of two objects that allow you to navigate and retrieve files using the xmlHttpRequest object
+	// The set of objects contains literal reg exp objects
+
+//	Needs the html document to get updated before any serious changes can be made.
+//	Needs to match other instances of the ktml js file.
+
+
+var dML = {	// dML  
+	"getData": /\w+(?=\|)/g,	// getData 
+	"getObject": /\{\w+\(\w+\)/g,	// getObject 
+	"getPart": /\w+(?=\.|\()/g,	// getPart 
+	"getPath": /\{\w+(?=\()/g,	// getPath 
+	"getElement": /\w+(?=\))/g	// getElement 
 }; 
 
 var tML = {
