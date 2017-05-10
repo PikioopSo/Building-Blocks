7e0fa1e4ab62e1b71b2d59f170b53cfb58ea49aa
Steven Van Sant <pireelanimation@gmail.com>
2017-05-10 21:04:28 +0000

    update

--- index-core.php
+++ index-core.php
@@ -11,7 +11,7 @@
     <meta name="keywords" content="computer science, cgi, json, json markup, web, social network, open source"></meta>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 
-    <link rel="stylesheet" href="/stylesheets/styles.css" type="text/css"></link>
+    <link rel="stylesheet" href="/stylesheets/main.css" type="text/css"></link>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
