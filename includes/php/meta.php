6b778db09c6a805493f9d2dbdaea7496393e16e3
KIP <kipomaha@gmail.com>
2017-06-02 05:00:09 -0600

    updates from local build
    changes to index.php, index-core.php, x-tag components, css, and others

---
+++ includes/php/meta.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace meta;
+
+function _meta_Require($metaJSON = "includes/json/meta.json") {
+    // Needs to check url parameter first
+    $_req = file_get_contents($metaJSON);
+    $_decode_ = json_decode($_req, true);
+    $_rt = print "<fire-base id='meta-JSON'>".$_req."</fire-base>";
+}
+
+function _meta_Library($libraryJSON = "lib/lib.json"){
+    // Needs to check url parameter first
+    $_req = file_get_contents($libraryJSON);
+    $_decode_ = json_decode($_req, true);
+    $_rt = print "<fire-base id='lib-JSON'>".$_req."</fire-base>";
+};
+
